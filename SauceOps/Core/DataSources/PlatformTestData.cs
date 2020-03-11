0d6a4a954742902af4a4cd73bf13abeea6f93f2f
Andrew Gray <andrew.paul.gray@gmail.com>
2020-03-11 11:58:10 +1100

    add another unit test

--- SauceOps/Core/DataSources/PlatformTestData.cs
+++ SauceOps/Core/DataSources/PlatformTestData.cs
@@ -10,7 +10,8 @@ namespace SauceOps.Core.DataSources
             get
             {
                 //Mobile Platforms
-                yield return new SaucePlatform("android", "chrome", "62.0", "android", "Google Pixel 3 GoogleAPI Emulator", "10.0.", "", "android", "landscape");
+                yield return new SaucePlatform("android", "android", "chrome", "62.0", "Google Pixel 3 GoogleAPI Emulator", "10.0.", "", "android", "landscape");
+                yield return new SaucePlatform("ios", "ios", "safari", "13.0", "iPhone XS Max Simulator", "13.0", "", "iphone", "portrait");
             }
         }
     }
---
+++ UnitTests/IOSFactoryVersionTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using SauceOps.Core.OnDemand;
+using SauceOps.Core.Capabilities;
+using Shouldly;
+using System.Collections;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class IOSFactoryVersionTests : TestBase
+    {
+        //[Test, TestCaseSource(typeof(AndroidDataClass), "SupportedTestCases")]
+        //public void IsSupportedPlatformTest(SaucePlatform saucePlatform)
+        //{
+        //    var caps = CapabilityFactory.CreateCapabilities(saucePlatform, "MyTest");
+        //    caps.IsSupportedPlatform().ShoulBeTrue();
+        //}
+
+        //[Test, TestCaseSource(typeof(AndroidDataClass), "NotSupportedTestCases")]
+        //public void IsNotSupportedPlatformTest(SaucePlatform saucePlatform)
+        //{
+        //    var factory = new OptionFactory(saucePlatform);
+        //    var result = factory.IsSupportedPlatform();
+        //    result.ShouldBeFalse();
+        //}
+
+        [Test, TestCaseSource(typeof(IOSDataClass), "SupportedTestCases")]
+        public void AppiumIOSCapabilityTest(SaucePlatform saucePlatform)
+        {
+            CapabilityFactory.CreateCapabilities(saucePlatform, "MyTest").ShouldNotBeNull();
+        }
+    }
+    public class IOSDataClass
+    {
+        public static IEnumerable SupportedTestCases
+        {
+            get
+            {
+                yield return new SaucePlatform("ios", "ios", "safari", "13.0", "iPhone XS Max Simulator", "13.0", "", "iphone", "portrait");
+            }
+        }
+
+        //public static IEnumerable NotSupportedTestCases
+        //{
+        //    get
+        //    {
+        //        yield return new TestCaseData(new SaucePlatform("android", "android", "android", "10.0", "Google Pixel 3 GoogleAPI Emulator", "10.0.", "", "android", "landscape"));
+        //    }
+        //}
+    }
+}
