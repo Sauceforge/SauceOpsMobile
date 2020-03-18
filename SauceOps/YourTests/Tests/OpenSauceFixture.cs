264ab25f6cdc747ac484394590ee3120ba7834d5
Andrew Gray <andrew.gray@coles.com.au>
2020-03-18 12:51:06 +1100

    clean up 2

--- SauceOps/YourTests/Tests/OpenSauceFixture.cs
+++ SauceOps/YourTests/Tests/OpenSauceFixture.cs
@@ -6,34 +6,26 @@ using YourTests.PageObjects;
 using Shouldly;
 
 namespace YourTests.Tests {
-    //[Parallelizable(ParallelScope.Children)]
     public class OpenSauceFixture : SauceryBase {
         public OpenSauceFixture(SaucePlatform platform) : base(platform) {
-            //Console.WriteLine("In GuineaPigFixture constructor");
         }
 
-        //[MethodImpl(MethodImplOptions.Synchronized)]
         [Test]
         [TestCase(5)]
         [TestCase(4)]
-        //[Ignore("Need OpenSauce")]
+        //[Ignore("Reason")]
         public void DoSomethingOnAWebPageWithSelenium(int data) {
-            //Console.WriteLine("In DoSomethingOnAWebPageWithSelenium");
             //Saucery supports NUnit data-driven tests 
             Console.WriteLine(@"My data is: " + data);
-            //Console.WriteLine(Driver== null? "Driver is null": "Driver good");
             var guineaPigPage = new GuineaPigPage(Driver, "https://saucelabs.com/");
 
             // verify the page title is correct - this is actually checked as part of the constructor above.
             Driver.Title.ShouldContain("I am a page title - Sauce Labs");
         }
 
-        //[MethodImpl(MethodImplOptions.Synchronized)]
         [Test]
-        //[Ignore("Need OpenSauce")]
+        //[Ignore("Reason")]
         public void DoSomethingElseOnAWebPageWithSelenium() {
-            //Console.WriteLine("In DoSomethingElseOnAWebPageWithSelenium");
-            //Console.WriteLine(Driver == null ? "Driver is null" : "Driver good");
             var guineaPigPage = new GuineaPigPage(Driver, "https://saucelabs.com/");
             // find and click the link on the page
             guineaPigPage.ClickLink();
@@ -42,12 +34,9 @@ namespace YourTests.Tests {
             Driver.Url.ShouldContain("saucelabs.com/test-guinea-pig2.html");
         }
 
-        //[MethodImpl(MethodImplOptions.Synchronized)]
         [Test]
-        //[Ignore("Need OpenSauce")]
+        //[Ignore("Reason")]
         public void DoSomethingElseAgainOnAWebPageWithSelenium() {
-            //Console.WriteLine("In DoSomethingElseAgainOnAWebPageWithSelenium");
-            //Console.WriteLine(Driver == null ? "Driver is null" : "Driver good");
             var guineaPigPage = new GuineaPigPage(Driver, "https://saucelabs.com/");
 
             // read the useragent string off the page
