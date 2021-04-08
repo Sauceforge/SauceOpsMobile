13a67d1adeea03ac8fbc679f486b7eec71389152
Andrew Gray <andrew.gray@coles.com.au>
2021-04-08 22:41:50 +1000

    update platforms

--- SauceOps/Core/DataSources/PlatformTestData.cs
+++ SauceOps/Core/DataSources/PlatformTestData.cs
@@ -10,7 +10,7 @@ namespace SauceOps.Core.DataSources
             get
             {
                 //Mobile Platforms
-                yield return new SaucePlatform("android", "android", "chrome", "89.0", "Google Pixel 3 GoogleAPI Emulator", "11.0.", "", "android", "landscape");
+                yield return new SaucePlatform("android", "android", "chrome", "89.0", "Google Pixel 3 GoogleAPI Emulator", "11.0.", "", "android", "portrait");
                 yield return new SaucePlatform("", "", "Safari", "14.3", "iPhone 12 Pro Simulator", "14.3", "", "iphone", "portrait");
             }
         }
